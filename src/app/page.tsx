1675d94d8b6902de19f6788608bdf0e199e137cc
gloomenn <gloomenn@yahoo.com>
2026-08-27 00:28:31 -0700

    update: cambios minimos en ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-gray-100">
-      <div className="text-center p-8">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">
-          🚀 Gestor de Trámites
+    <main className="min-h-screen flex items-center justify-center bg-black "> 
+      <div className="text-center ">
+        <h1 className="text-6xl font-bold text-white mt-2 mb-10 hover:scale-105 transition-transform duration-300">
+          Bienvenido a <span className="text-purple-600">Boreal</span>
         </h1>
         <p className="text-gray-600 mb-6">
           Administra tus correos temporales de forma fácil y rápida
@@ -14,13 +14,13 @@ export default function Home() {
         <div className="space-x-4">
           <Link
             href="/login"
-            className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
+            className="bg-purple-600 text-white px-6 py-3 rounded-lg hover:bg-pink-700 active:bg-white-600 transition"
           >
             Iniciar Sesión
           </Link>
           <Link
             href="/register"
-            className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700 transition"
+            className="bg-gray-600 text-white px-6 py-3 rounded-lg hover:bg-gray-400 active:bg-white-600 transition"
           >
             Registrarse
           </Link>
@@ -29,3 +29,7 @@ export default function Home() {
     </main>
   );
 }
+
+
+
+
